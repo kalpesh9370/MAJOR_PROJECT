47949577a6250a3289335d170a45ad2d629d46dc
Kalpesh <patilkalpeshuchanda@gmail.com>
2024-09-22 19:48:55 +0530

    booking feature added4

--- routes/booking.js
+++ routes/booking.js
@@ -46,7 +46,6 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing");
 const Booking = require("../models/booking"); // Ensure this model is correctly defined
-const Booking = require("../models/booking.js"); // Ensure this model is correctly defined
 const { isLoggedIn } = require("../middleware");
 
 // Route to show booking form
